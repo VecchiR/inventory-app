d354a91b12e3726391fede1dc641257f7aee4a3e
Vecchi <79600745+VecchiR@users.noreply.github.com>
2025-02-21 10:41:41 -0300

    Implement game tags on game creation and updating

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,5 +1,6 @@
 const gamesDb = require('../db/gameQueries');
 const platformsDb = require('../db/platformQueries');
+const tagsDb = require('../db/tagQueries');
 const links = require('../routes/links');
 
 async function getAllGames(req, res) {
@@ -10,19 +11,23 @@ async function getAllGames(req, res) {
 async function getGame(req, res) {
     const game = await gamesDb.getGame(req.params.gameId);
     const gamePlatforms = await gamesDb.getGamePlatforms(req.params.gameId);
-    res.render('game_views/gameDetails', { game: game[0], gamePlatforms });
+    const gameTags = await gamesDb.getGameTags(req.params.gameId);
+    res.render('game_views/gameDetails', { game: game[0], gamePlatforms, gameTags });
 };
 
 async function updateGameGet(req, res) {
     const game = await gamesDb.getGame(req.params.gameId);
     const platforms = await platformsDb.getAllPlatforms();
+    const tags = await tagsDb.getAllTags();
     const gamePlatforms = await gamesDb.getGamePlatforms(req.params.gameId);
-    res.render('game_views/updateGameForm', { title: "Edit game", game: game[0], platforms, gamePlatforms });
+    const gameTags = await gamesDb.getGameTags(req.params.gameId);
+    res.render('game_views/updateGameForm', { title: "Edit game", game: game[0], platforms, gamePlatforms, tags, gameTags });
 };
 
 async function createGameGet(req, res) {
     const platforms = await platformsDb.getAllPlatforms();
-    res.render('game_views/newGameForm', { title: 'New game form', platforms });
+    const tags = await tagsDb.getAllTags();
+    res.render('game_views/newGameForm', { title: 'New game form', platforms, tags });
 };
 
 async function createGamePost(req, res) {
@@ -41,6 +46,15 @@ async function createGamePost(req, res) {
         else if (req.body.platforms) {
             await gamesDb.insertGamePlatform(gameId, Number(req.body.platforms));
         }
+        
+        if (Array.isArray(req.body.tags)) {
+            req.body.tags.map(async tag => await gamesDb.insertGameTag(gameId, Number(tag)));
+
+        }
+        else if (req.body.tags) {
+            await gamesDb.insertGameTag(gameId, Number(req.body.tags));
+        }
+
 
         res.redirect('/');
     }
@@ -60,33 +74,24 @@ async function updateGamePost(req, res) {
 
 
     const checkedPlatforms = [];
+    const checkedTags = [];
 
-    if (!req.body.platforms) {
-        // somethign
-    }
 
-    else if (Array.isArray(req.body.platforms)) {
+    if (Array.isArray(req.body.platforms)) {
         req.body.platforms.forEach(p => checkedPlatforms.push(Number(p)));
     }
-    else { checkedPlatforms.push(Number(req.body.platforms)); }
+    else if (req.body.platforms) { checkedPlatforms.push(Number(req.body.platforms)); }
 
-
-    console.log(checkedPlatforms);
-
-
-
-
-
-
-    // await gamesDb.test(gameId, checkedPlatforms);
-    // res.redirect(`/game/${gameId}/update`);
-
+    if (Array.isArray(req.body.tags)) {
+        req.body.tags.forEach(t => checkedTags.push(Number(t)));
+    }
+    else if(req.body.tags) { checkedTags.push(Number(req.body.tags)); }
 
 
     await gamesDb.insertGamePlatformsForUpdate(gameId, checkedPlatforms);
+    await gamesDb.insertGameTagsForUpdate(gameId, checkedTags);
 
-    res.redirect(`/game/${gameId}/update`);
-    // res.redirect(`/game/${gameId}`);
+    res.redirect(`/game/${gameId}`);
 };
 
 async function deleteGamePost(req, res) {
